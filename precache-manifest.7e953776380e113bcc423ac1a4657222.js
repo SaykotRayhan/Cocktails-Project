ab29fcf7fa98b8847624c10924f6dfb4661364fc
Rayhan <saykotrayhan199@gmail.com>
2022-09-07 23:11:37 +0600

    Updates

---
+++ precache-manifest.7e953776380e113bcc423ac1a4657222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bc3cc9a29c25d249d6881d9e4f04d0",
+    "url": "/Cocktails-Project/index.html"
+  },
+  {
+    "revision": "e8b92ac538777b1b67d0",
+    "url": "/Cocktails-Project/static/css/main.0a967299.chunk.css"
+  },
+  {
+    "revision": "733d6bc7ee5bfdab501f",
+    "url": "/Cocktails-Project/static/js/2.ff662081.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/Cocktails-Project/static/js/2.ff662081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b92ac538777b1b67d0",
+    "url": "/Cocktails-Project/static/js/main.d62104d3.chunk.js"
+  },
+  {
+    "revision": "5eb7ea3e91500172a0be",
+    "url": "/Cocktails-Project/static/js/runtime-main.b2353420.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/Cocktails-Project/static/media/logo.9a3d2645.svg"
+  }
+]);
